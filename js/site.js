8ae7c06ca6e2157c1f8ad6c49f252171dba9803e
josephStuff <93104277+josephStuff@users.noreply.github.com>
2022-01-21 20:45:06 -0500

    some HTML
    needs a table

--- js/site.js
+++ js/site.js
@@ -1,14 +1,2 @@
 
 
-
-// display message to the user.
-
-function getMessage() {
-    let userMessage = document.getElementById("message").value;
-    Swal.fire(
-        'Button clicked!',
-        userMessage,
-        "success"
-    );
-    console.log(userMessage);
-}
