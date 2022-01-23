976ba34eb162fb3f9b5cff81756e1c711b7d998e
josephStuff <93104277+josephStuff@users.noreply.github.com>
2022-01-23 02:07:54 -0500

    making progress
    still needs formulas

--- js/site.js
+++ js/site.js
@@ -1,8 +1,29 @@
 
 
-function getValues () {
-    let loanAmount = 0;
-    let interest = 0;
-    let term = 0;
+function getUserInput () {
+
+    let loanAmount = document.getElementById("loanAmount").value;
+    let interestRate = document.getElementById("interestRate").value;
+    let howManyMonths = document.getElementById("howManyMonths").value;
     
+    loanAmount = parseInt(loanAmount);
+    interestRate = parseInt(interestRate);
+    howManyMonths = parseInt(howManyMonths);
+
+    // check if they are in fact INTEGERS !
+
+    if(Number.isInteger(loanAmount, interestRate, howManyMonths)) {
+
+        
+
+        // else {
+        // Swal.fire({
+        // icon: 'error',
+        // title: 'Ooops...',
+        // text: 'Only integers are allowed for Hundo'
+        //  })
+
+    }
+
+
 }
