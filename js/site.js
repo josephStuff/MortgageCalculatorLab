9fcadb8e9c3009e67b3b8320379427cd9f97a634
josephStuff <93104277+josephStuff@users.noreply.github.com>
2022-01-23 18:29:23 -0500

    still need correct monthly pmt

--- js/site.js
+++ js/site.js
@@ -1,46 +1,53 @@
 
 
-function getUserInput () {
+
+function getUserInput() {
 
     let loanAmount = document.getElementById("loanAmount").value;
     let interestRate = document.getElementById("interestRate").value;
     let howManyMonths = document.getElementById("howManyMonths").value;
-    
+    let monthlyTotal = document.getElementById("monthlyTotal").value;
+
+
     loanAmount = parseInt(loanAmount);
     interestRate = parseInt(interestRate);
     howManyMonths = parseInt(howManyMonths);
 
     // check if they are INTEGERS !
 
-    if(Number.isInteger(loanAmount, interestRate, howManyMonths)) {
+    if (Number.isInteger(loanAmount, interestRate, howManyMonths)) {
 
-        console.log("heidy ho!");
+        interestRate = interestRate / 100;
 
-        // interestRate = percentToDecimal(interestRate);
-        
-        // return (interestRate * loanAmount) / (1 - (Math.pow((1 + interestRate), (-howManyMonths))));
+        monthlyTotal = (interestRate * loanAmount) / (1 - Math.pow((1 + interestRate), (-howManyMonths)));
 
+    }
 
-    }        
+    else {
+        Swal.fire({
+        icon: 'error',
+        title: 'Ooops...',
+        text: 'Only integers are allowed for calculation'});
 
+    }
 
+    // console.log(monthlyTotal);
+
+    document.getElementById("monthlyTotal").innerHTML = monthlyTotal.toLocaleString();
 
 }
 
-function displayPayment () {
+function displayPayment() {
 
-    let totalPrincipal = 0;
-    let totalInterest = 0;
-    let totalCost = 0;
+  let totalPrincipal = parseInt(document.getElementById("loanAmount").value);
+  let totalInterest = (document.getElementById("interestRate").value / 100) * (document.getElementById("loanAmount").value);
+  let totalCost = totalPrincipal + totalInterest;
 
-    
+
+
+  document.getElementById("totalPrincipal").innerHTML = totalPrincipal.toLocaleString();
+  document.getElementById("totalInterest").innerHTML = totalInterest.toLocaleString();
+  document.getElementById("totalCost").innerHTML = totalCost.toLocaleString();
+
+
 }
-
-
-
-    // else {
-    // Swal.fire({
-    // icon: 'error',
-    // title: 'Ooops...',
-    // text: 'Only integers are allowed for calculation'
-    //     });
