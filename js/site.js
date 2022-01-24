08056202a7090f50918f32e4af7cfccd94502263
josephStuff <93104277+josephStuff@users.noreply.github.com>
2022-01-23 23:25:15 -0500

    corrected the total Interest
    no tables

--- js/site.js
+++ js/site.js
@@ -37,8 +37,9 @@ function getUserInput() {
 
 function displayPayment() {
 
+//   let monthlyTotal = getUserInput;  
   let totalPrincipal = parseInt(document.getElementById("loanAmount").value);
-  let totalInterest = (document.getElementById("interestRate").value / 100 / 12) * document.getElementById("howManyMonths").value * document.getElementById("loanAmount").value;  
+  let totalInterest = document.getElementById("monthlyTotal").innerHTML * document.getElementById("howManyMonths").value - document.getElementById("loanAmount").value;
   let totalCost = totalPrincipal + totalInterest;
 
 
@@ -49,34 +50,34 @@ function displayPayment() {
 
 }
 
-function displayToTable () {
+// function displayToTable () {
 
-    let debtBody = document.getElementById("debtBody");
-    let listNames_template = document.getElementById("listNames_template")
+//     let debtBody = document.getElementById("debtBody");
+//     let listNames_template = document.getElementById("listNames_template")
 
-    debtBody.innerHTML = "";
+//     debtBody.innerHTML = "";
 
-    let currentPayments = JSON.parse(localStorage.getItem("paymentArray")) || [];
+//     let currentPayments = JSON.parse(localStorage.getItem("paymentArray")) || [];
 
-    if (currentPayments == 0) {
-        currentPayments = debts;
-        localStorage.setItem("paymentArray", JSON.stringify(currentPayments));
-    }
+//     if (currentPayments == 0) {
+//         currentPayments = debts;
+//         localStorage.setItem("paymentArray", JSON.stringify(currentPayments));
+//     }
 
-    for (let index = 0; index < currentPayments.length; index++) {
-        // const element = array[index];
-        let paymentRow = document.importNode(listNames_template.content, true);
-        let paymentCols = paymentRow.querySelectorAll("td");
+//     for (let index = 0; index < currentPayments.length; index++) {
+//         // const element = array[index];
+//         let paymentRow = document.importNode(listNames_template.content, true);
+//         let paymentCols = paymentRow.querySelectorAll("td");
 
-        paymentCols[0].textContent = currentPayments[index].month;
-        paymentCols[1].textContent = currentPayments[index].payment;
-        paymentCols[2].textContent = currentPayments[index].principal;
-        paymentCols[3].textContent = currentPayments[index].interest;
-        paymentCols[4].textContent = currentPayments[index].totalInterest;
-        paymentCols[5].textContent = currentPayments[index].balance;
+//         paymentCols[0].textContent = currentPayments[index].month;
+//         paymentCols[1].textContent = currentPayments[index].payment;
+//         paymentCols[2].textContent = currentPayments[index].principal;
+//         paymentCols[3].textContent = currentPayments[index].interest;
+//         paymentCols[4].textContent = currentPayments[index].totalInterest;
+//         paymentCols[5].textContent = currentPayments[index].balance;
 
-        debtBody.appendChild(paymentRow);
-    }
+//         debtBody.appendChild(paymentRow);
+//     }
 
 
-}
+// }
