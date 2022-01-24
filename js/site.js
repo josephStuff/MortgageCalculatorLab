cdf09bed7c46bd83bf40cf612eff9f19de3408d5
josephStuff <93104277+josephStuff@users.noreply.github.com>
2022-01-23 20:34:57 -0500

    working on the table list
    list data to display.  not completed

--- js/site.js
+++ js/site.js
@@ -6,7 +6,7 @@ function getUserInput() {
     let loanAmount = document.getElementById("loanAmount").value;
     let interestRate = document.getElementById("interestRate").value;
     let howManyMonths = document.getElementById("howManyMonths").value;
-    let monthlyTotal = document.getElementById("monthlyTotal").value;
+     
 
 
     loanAmount = parseInt(loanAmount);
@@ -17,9 +17,10 @@ function getUserInput() {
 
     if (Number.isInteger(loanAmount, interestRate, howManyMonths)) {
 
-        interestRate = interestRate / 100;
+        interestRate = (interestRate / 100) / 12;
 
-        monthlyTotal = (interestRate * loanAmount) / (1 - Math.pow((1 + interestRate), (-howManyMonths)));
+        let monthlyTotal = (interestRate * loanAmount) / (1 - Math.pow((1 + interestRate), (-howManyMonths)));
+        document.getElementById("monthlyTotal").innerHTML = monthlyTotal.toLocaleString();
 
     }
 
@@ -27,27 +28,41 @@ function getUserInput() {
         Swal.fire({
         icon: 'error',
         title: 'Ooops...',
-        text: 'Only integers are allowed for calculation'});
+        text: 'Numbers Only Please!!!!'});
 
     }
 
-    // console.log(monthlyTotal);
-
-    document.getElementById("monthlyTotal").innerHTML = monthlyTotal.toLocaleString();
 
 }
 
 function displayPayment() {
 
   let totalPrincipal = parseInt(document.getElementById("loanAmount").value);
-  let totalInterest = (document.getElementById("interestRate").value / 100) * (document.getElementById("loanAmount").value);
+  let totalInterest = (document.getElementById("interestRate").value / 100 / 12) * document.getElementById("howManyMonths").value * document.getElementById("loanAmount").value;  
   let totalCost = totalPrincipal + totalInterest;
-
 
 
   document.getElementById("totalPrincipal").innerHTML = totalPrincipal.toLocaleString();
   document.getElementById("totalInterest").innerHTML = totalInterest.toLocaleString();
   document.getElementById("totalCost").innerHTML = totalCost.toLocaleString();
+
+
+}
+
+function displayToTable () {
+
+    let locationOfList = document.getElementById("debtBody");
+    let templateForList = document.getElementById("")
+
+
+
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        
+    }
+
+    let row = `<tr><td class="${className}">${number}</td></tr>`;
+    templateRows += row;
 
 
 }
